e7fa75338ded20b248dd734afe442c4ebb0273c9
Nauman-S <nauman843@hotmail.com>
2025-01-25 19:11:52 +0800

    testing deployment changes

--- ui/src/App.js
+++ ui/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Cloud Computing
+          Testing 123
         </a>
       </header>
     </div>
