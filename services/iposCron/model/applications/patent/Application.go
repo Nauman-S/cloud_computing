fbdeca4ac516a7fe201a3886bd98c95f0814f078
Nauman-S <e0389102@u.nus.edu>
2025-02-11 00:50:54 +0800

    fetch-data-cron boilerplate

--- services/iposCron/model/applications/patent/Application.go
+++ services/iposCron/model/applications/patent/Application.go
@@ -1,9 +1,10 @@
 package patent
 
 type Application struct {
-	Summary             ApplicationSummary `json:"summary" bson:"summary"`
-	Applicants          []Applicant        `json:"applicant" bson:"applicant"`
-	HmgStatus           string             `json:"hmgStatus" bson:"hmgStatus"`
-	TransferOfOwnership string             `json:"transferOfOwnership" bson:"transferOfOwnership"`
-	Inventors           []Inventor         `json:"inventors" bson:"inventors"`
+	Summary                 ApplicationSummary      `json:"summary" bson:"summary"`
+	Applicants              []Applicant             `json:"applicant" bson:"applicant"`
+	HmgStatus               string                  `json:"hmgStatus" bson:"hmgStatus"`
+	TransferOfOwnership     string                  `json:"transferOfOwnership" bson:"transferOfOwnership"`
+	Inventors               []Inventor              `json:"inventors" bson:"inventors"`
+	DeclarationOfPriorities []DeclarationOfPriority `json:"declarationOfPriority" bson:"declarationOfPriority"`
 }
