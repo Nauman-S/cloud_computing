24c5f9e2e432ef046a53ae60ff68f6d09ba1f11e
Nauman-S <e0389102@u.nus.edu>
2025-02-11 00:45:01 +0800

    fetch-data-cron boilerplate

--- services/iposCron/model/applications/patent/Inventor.go
+++ services/iposCron/model/applications/patent/Inventor.go
@@ -1,8 +1,8 @@
 package patent
 
 type Inventor struct {
-	Name               string `json:"name" bson:"name"`
-	Address            string `json:"address" bson:"address"`
-	CountryOfResidence string `json:"countryOfResidence" bson:"countryOfResidence"`
-	Nationality        string `json:"nationality" bson:"nationality"`
+	Name               string        `json:"name" bson:"name"`
+	Address            string        `json:"address" bson:"address"`
+	CountryOfResidence CountryDetail `json:"countryOfResidence" bson:"countryOfResidence"`
+	Nationality        string        `json:"nationality" bson:"nationality"`
 }
