167bc2bffce9040892d03b879e8bd999540a33fe
Nauman-S <e0389102@u.nus.edu>
2025-02-23 05:03:42 +0800

    Document Field

---
+++ services/ipos/src/main/java/com/cs5224/ipos/model/documents/Document.java
@@ -0,0 +1,23 @@
+package com.cs5224.ipos.model.documents;
+
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Data
+public class Document {
+
+    @Field("fileName")
+    String fileName;
+
+    @Field("lodgementDate")
+    String lodgementDate;
+
+    @Data
+    public static class DocType {
+        @Field("description")
+        private String description;
+
+        @Field("code")
+        private String code;
+    }
+}
