b6e7e090b1070c93bad9f3b3ee906ab7a524cdc0
Nauman-S <nauman843@hotmail.com>
2025-04-02 23:25:19 +0800

    oauth2 google

--- ui/src/components/oAuth2/OAuth.js
+++ ui/src/components/oAuth2/OAuth.js
@@ -1,9 +1,26 @@
 import URLs from "../../constants/urls";
 import axios from "axios";
 
-const googleLogin = () => {
-    console.log("Google login");
-}
+const googleLogin = async () => {
+    try {
+        const response = await axios.post(URLs.OAUTH_SET_REDIRECT,
+        {
+            redirectUrl: window.location.origin + "/status",
+        },
+        {
+            withCredentials: true, // Ensure cookies are included
+        });
+
+        if (response.status === 200) {
+            window.location.href = URLs.OAUTH_GOOGLE;
+        } else {
+            console.log(response);
+        }
+
+    } catch (error) {
+        console.error("Error Configuring Redirect:", error);
+    }
+};
 
 const githubLogin = async () => {
     try {
@@ -17,6 +34,8 @@ const githubLogin = async () => {
 
         if (response.status === 200) {
             window.location.href = URLs.OAUTH_GITHUB;
+        } else {
+            console.log(response);
         }
       } catch (error) {
         console.error("Error Configuring Redirect:", error);
