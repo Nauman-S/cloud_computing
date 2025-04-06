de6fe27cb904955ca55cf197be22ca4c4ff7c516
snehakumaarr <snehakumaarr@gmail.com>
2025-04-06 18:23:08 +0800

    APIs for Search

--- services/ipos/src/main/java/com/cs5224/ipos/controller/PatentController.java
+++ services/ipos/src/main/java/com/cs5224/ipos/controller/PatentController.java
@@ -2,6 +2,8 @@ package com.cs5224.ipos.controller;
 
 import com.cs5224.ipos.command.PatentAggregationCommand;
 import com.cs5224.ipos.command.PatentCommand;
+import com.cs5224.ipos.command.PatentSearchCommand; 
+import com.cs5224.ipos.dto.PatentSearchRequest; 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +21,9 @@ public class PatentController {
     @Autowired
     PatentAggregationCommand patentAggregationCommand;
 
+    @Autowired
+    private PatentSearchCommand patentSearchCommand;
+
     @GetMapping
     public ResponseEntity<?> patentTemplateQuery(@RequestParam(required=false) String groupBy,
                                                    @RequestParam(required = false) String aggregate) {
@@ -31,4 +36,28 @@ public class PatentController {
 
         return patentCommand.execute(applicationNumber);
     }
+
+    @GetMapping("/search")
+    public ResponseEntity<?> searchPatents(
+            @RequestParam(required = false) String applicationNum,
+            @RequestParam(required = false) String applicationType,
+            @RequestParam(required = false) String titleOfInvention,
+            @RequestParam(required = false) String filingDateStart,
+            @RequestParam(required = false) String filingDateEnd,
+            @RequestParam(required = false) String lodgementDateStart,
+            @RequestParam(required = false) String lodgementDateEnd
+    ) {
+        PatentSearchRequest searchRequest = new PatentSearchRequest();
+        searchRequest.setApplicationNum(applicationNum);
+        searchRequest.setApplicationType(applicationType);
+        searchRequest.setTitleOfInvention(titleOfInvention);
+        searchRequest.setFilingDateStart(filingDateStart);
+        searchRequest.setFilingDateEnd(filingDateEnd);
+        searchRequest.setLodgementDateStart(lodgementDateStart);
+        searchRequest.setLodgementDateEnd(lodgementDateEnd);
+        // titleEmbedding and documentEmbedding can be set here if needed in the future
+
+        return patentSearchCommand.execute(searchRequest);
+    }
+
 }
